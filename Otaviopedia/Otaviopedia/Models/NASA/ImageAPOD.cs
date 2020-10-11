411865fd23981e5386ad4175eae70c6eee86e202
Otávio Azevedo <otavioazevedo7@gmail.com>
2020-10-10 23:26:49 -0300

    Initial commit.

---
+++ Otaviopedia/Otaviopedia/Models/NASA/ImageAPOD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Otaviopedia.Models.NASA
+{
+    public class ImageAPOD
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string MediaType { get; set; }
+        public string Copyright { get; set; }
+        public string SourceAPI { get; set; }
+    }
+}
+
---
+++ Otaviopedia/Otaviopedia/Models/NASA/NASAModel.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Otaviopedia.Models.NASA
+{
+    public class NasaModel
+    {
+        /*TODO, why dont work in .net core 3.1?
+        private readonly IConfiguration _config;
+		*/
+        public ImageAPOD GetAPOD()
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string baseURL = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("NASA_OpenAPIs")["BaseURL"]; ;
+                string key = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("NASA_OpenAPIs")["Key"]; ;
+
+                DateTime dataBase = DateTime.Now.Date.AddDays(
+                    new Random().Next(0, 7) * -1);
+
+                HttpResponseMessage response = client.GetAsync(
+                    baseURL + "apod?" +
+                    $"api_key={key}&" +
+                    $"date={dataBase.ToString("yyyy-MM-dd")}").Result;
+
+                response.EnsureSuccessStatusCode();
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+
+                dynamic resultado = JsonConvert.DeserializeObject(conteudo);
+
+                ImageAPOD image = new ImageAPOD();
+                image.Date = dataBase;
+                image.SourceAPI = "https://api.nasa.gov";
+                image.Title = resultado.title;
+                image.Description = resultado.explanation;
+                image.Url = resultado.url;
+                image.MediaType = resultado.media_type;
+                image.Copyright = resultado.copyright;
+
+                return image;
+            }
+        }
+    }
+}
